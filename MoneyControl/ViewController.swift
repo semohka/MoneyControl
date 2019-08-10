dfe9e38b1dc35fb36dfec3fab3ff3b31ee95639f
Kristina <semohka@yandex.ru>
2019-08-10 21:56:15 +0300

    Initial Commit

---
+++ MoneyControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoneyControl
+//
+//  Created by Кристина Семкова on 10/08/2019.
+//  Copyright © 2019 Кристина Семкова. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
